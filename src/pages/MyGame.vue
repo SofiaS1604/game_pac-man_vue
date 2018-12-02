e0ad802bcc10f176651ccc0f12bd26159cf96913
SofiaS1604 <SofiaS1604@yandex.ru>
2018-12-02 19:59:45 +0300

    Add files via upload

--- src/pages/MyGame.vue
+++ src/pages/MyGame.vue
@@ -13,22 +13,34 @@
         <button v-if="!isShowing" class="button button_right">Result</button>
         
         <transition name="grid">
-            <div class="grid" v-if="isShowing">
+            <div class="grid" v-if="isShowing" >
+               
+                <my-grid v-if="isShowing"></my-grid>
+                 <div class="square"  :style="square"></div>
 
                 <div  class="row" v-for="n in 36">
                     <div class="cell" v-for="k in 46"></div>
                 </div>
 
-                <div v-if="isShowing" class="circle" :style="circleStyle_1"></div>
-                <!--<div v-if="isShowing" class="circle" :style="circleStyle_2"></div>-->
-                <!--<div v-if="isShowing" class="circle" :style="circleStyle_3"></div>-->
-                <!--<div v-if="isShowing" class="circle" :style="circleStyle_4"></div>-->
-                <!--<div v-if="isShowing" class="circle" :style="circleStyle_5"></div>-->
+                <div class="" v-if="isShowing"> 
+                    <div v-if="CircleShowing_1" class="circle circle_1" :style="circleStyle_1"></div>
+                </div>
+                <div class="" v-if="isShowing"> 
+                    <div v-if="CircleShowing_2" class="circle circle_2" :style="circleStyle_2"></div>
+                </div>
+                <div class="" v-if="isShowing"> 
+                    <div v-if="CircleShowing_3" class="circle circle_3" :style="circleStyle_3"></div>
+                </div>
+                <div class="" v-if="isShowing"> 
+                    <div v-if="CircleShowing_4" class="circle circle_4" :style="circleStyle_4"></div>
+                </div>
+                <div class="" v-if="isShowing"> 
+                    <div v-if="CircleShowing_5" class="circle circle_5" :style="circleStyle_5"></div>
+                </div>
 
             <!-- Green Square-->
-                <div class="square"  :style="square"></div> 
+                 
 
-                <my-grid v-if="isShowing"></my-grid>
             </div>
         </transition>
     </div> 
@@ -53,10 +65,15 @@
                 cell:  46, //  width 
                 row: 36,//  height
                 isShowing: false,   
+                CircleShowing_1: false,
+                CircleShowing_2: false,
+                CircleShowing_3: false,
+                CircleShowing_4: false,
+                CircleShowing_5: false,
                 m: null, //minute
                 s: null, //second
                 score: 0,
-                seconds: 59, 
+                seconds: 120, 
                 circleX_1: null,
                 circleY_1: null,
                 circleX_2: null,
@@ -75,70 +92,204 @@
         
         methods: {
             toggleShow() {
+                this.CircleShowing_1 = !this.CircleShowing_1;
+                this.CircleShowing_2 = !this.CircleShowing_2;
+                this.CircleShowing_3 = !this.CircleShowing_3;
+                this.CircleShowing_4 = !this.CircleShowing_4;
+                this.CircleShowing_5 = !this.CircleShowing_5;
                 this.isShowing = !this.isShowing;
+                
+                
                 if (this.m === null ) {
                     this.timerStart();
+                    this.drawCircle();
+                    this.gameScore();
                 }
                 
                 
             }, //disappearance and time
 
             timerStart(){
-                if(this.seconds - 1 >= 0){
+
+                
+                if (this.seconds > 0) {
+                    this.seconds--;
                     this.m = parseInt(this.seconds / 60) + '';
-                    this.seconds --;
-                    if(this.m.length === 1){
-                        this.m = '0' + this.m;
+                    if (this.m.length === 1) {
+                      this.m = '0' + this.m;
                     }
                     this.s = this.seconds - parseInt(this.seconds / 60) * 60 + '';
-                    if(this.s.length === 1){
-                        this.s = '0' + this.s;
+                    if (this.s.length === 1) {
+                      this.s = '0' + this.s;
                     }
                     setTimeout(() => {
-                        this.timerStart();
-                       
-                        this.circleX_1 = Math.floor(Math.random() * 46);
-                        this.circleY_1 = Math.floor(Math.random() * 36);
-                       
+                      this.timerStart();
                     }, 1000);
+        
+                    
                     
                 }else{
-                    this.gameOver()
+                    this.gameOver();
+
+                    
                 }
             },
 
+            drawCircle(){
+
+                this.circleX_1 = Math.floor(Math.random() * 46);
+                this.circleX_2 = Math.floor(Math.random() * 46);
+                this.circleX_3 = Math.floor(Math.random() * 46);
+                this.circleX_4 = Math.floor(Math.random() * 46);
+                this.circleX_5 = Math.floor(Math.random() * 46);
+                this.circleY_5 = Math.floor(Math.random() * 36);
+                this.circleY_1 = Math.floor(Math.random() * 36);
+                this.circleY_2 = Math.floor(Math.random() * 36);
+                this.circleY_3 = Math.floor(Math.random() * 36);
+                this.circleY_4 = Math.floor(Math.random() * 36);
+                
+                setTimeout(() => {
+                   this.drawCircle();
+                   this.CircleShowing_1 = true;
+                   this.CircleShowing_2 = true;
+                   this.CircleShowing_3 = true;
+                   this.CircleShowing_4 = true;
+                   this.CircleShowing_5 = true;
+                   this.gameBlock();
+                   
+                }, 5000)
+            },
+
+            gameBlock(){
+                var wall = document.getElementsByClassName('wall_grid');
+                console.info(wall);
+                
+            },
+
+            gameScore(){
+                
+                if(this.circleX_1*15 === this.x*15 && this.circleY_1*15 === this.y*15){
+                    if(this.CircleShowing_1 === true){
+                        this.score += 1;
+                        this.CircleShowing_1 = false;
+                    }
+                }
+
+                if(this.circleX_2*15 === this.x*15 && this.circleY_2*15 === this.y*15){
+                    if(this.CircleShowing_2 === true){
+                        this.score += 1;
+                        this.CircleShowing_2 = false;
+                    }else{
+                        this.score += 0;
+                    }
+               }
+
+                if(this.circleX_3*15 === this.x*15 && this.circleY_3*15 === this.y*15){
+                    if(this.CircleShowing_3 === true){
+                        this.score += 1;
+                        this.CircleShowing_3 = false;
+                    }   
+                }
+
+                if(this.circleX_4*15 === this.x*15 && this.circleY_4*15 === this.y*15){
+                    if(this.CircleShowing_4 === true){
+                        this.score += 1;
+                        this.CircleShowing_4 = false;
+                    }
+                }
+
+                if(this.circleX_5*15 === this.x*15 && this.circleY_5*15 === this.y*15){
+                    if(this.CircleShowing_5 === true){
+                        this.score += 1;
+                        this.CircleShowing_5 = false;
+                    }
+                }
+
+                setTimeout(() => {
+                    this.gameScore()
+                },10)
+
+                
+            },
+
             gameOver(){
-                this.isShowing = false;
+                this.isShowing = false; 
+                     
             },
 
             left(){
                 if(this.x > 0){
-                    this.x --;
+                    if(this.x === 23 && (this.y === 3 || this.y === 4) || 
+                    this.x === 10 && this.y < 10 ||
+                    this.x === 20 && (this.y === 9 || this.y === 8) ||
+                    this.x === 30 && this.y < 8 ||
+                    this.x === 35  && (this.y > 4 && this.y < 10) ||
+                    this.x === 43  && (this.y > 2 && this.y < 25)){
+
+                    }else{
+                        this.x --;
+                    }
+                    
                 }
             }, //move left
 
             right(){
                 if(this.x < this.cell-1){
-                    this.x ++;
+                    if(this.x === 7 && this.y < 10 ||
+                     this.x === 2 && (this.y === 3 || this.y === 4) ||
+                     this.x === 12 && (this.y === 3 || this.y === 4) || 
+                     this.x === 27 && this.y < 8 ||
+                     this.x === 32  && (this.y > 2 && this.y < 10) ||
+                     this.x === 40  && (this.y > 4 && this.y < 25) ||
+                     this.x === 12  && (this.y > 9 && this.y < 18) ||
+                     this.x === 7  && (this.y === 18 || this.y === 19) ||
+                     this.x === 17  && (this.y > 17 && this.y < 25)){
+
+                    }else{
+                        this.x ++;
+                    }
+
+                    
                 }
             }, //move right
 
              top(){
                 if(this.y > 0){
-                    this.y --;
+                    if((this.x > 2 && this.x < 8) && this.y === 5 || 
+                    (this.x > 7 && this.x < 20) && (this.y < 11 && this.y > 9) ||
+                    (this.x > 12 && this.x < 23)  && this.y === 5 ||
+                    (this.x === 28 || this.x === 29) && this.y === 8 ||
+                    this.y === 10  && (this.x === 33 || this.x === 34) ||
+                    this.y === 5  && (this.x > 34 && this.x < 42) ||
+                    this.x > 40   &&  (this.y < 26 && this.y > 23) ||
+                    (this.x > 7 && this.x < 20)  && this.y === 20 ||
+                    (this.x === 18  ||  this.x === 19) &&  this.y === 25){
+
+                    }else{
+                        this.y --;
+                    }
+                    
                 }
              }, //move top
 
              bottom(){
                 if(this.y < this.row-1){
-                    this.y ++;
+                    if((this.x > 2 && this.x < 8)  && this.y === 2 || 
+                    (this.x > 12 && this.x < 23)  && this.y === 2 || 
+                    (this.x > 32 && this.x < 43)  && this.y === 2 ||
+                    this.y === 7 && (this.x > 9 && this.x < 20) ||
+                    this.x > 41 && this.y === 22 ||
+                    (this.x > 7 && this.x < 13)  && this.y === 17){
+
+                    }else{
+                        this.y ++;
+                    }
+                    
                 }
              }, //move bottom
              
              previous: function (e) {
-                //console.info(green_kvadrat_left)
-                //const green_kvadrat_left = this.$refs.kvadrat.getBoundingClientRect().left; 
-                
+
                 switch(e.keyCode){
                     case 37: //left arrow
                         this.left();
@@ -153,37 +304,59 @@
                         this.bottom()
                         break;         
                 }
-               
             },
-
         },
          
-     
         mounted() {
             window.addEventListener('keydown',  this.previous) // the number of the pressed button on the keyboard is checked by the method prevision
         },
 
         computed:{
             square(){
-                const green_square = {};
-            //    console.info(green_square)  
+                const green_square = {};  
                 green_square.left = this.x*15; //left
                 green_square.top = this.y*15; //top
                 return green_square; 
-
             },
 
             circleStyle_1(){
                 const red_circle = {};
-                
                 red_circle.left = this.circleX_1*15;
-                console.info(red_circle.left);
                 red_circle.top  = this.circleY_1*15;
+                return red_circle;    
+            },
+
+            circleStyle_2(){
+                const red_circle = {};
+                red_circle.left = this.circleX_2*15;
+                red_circle.top  = this.circleY_2*15;
+                return red_circle;
+            },
+
+            circleStyle_3(){
+                const red_circle = {};
+                red_circle.left = this.circleX_3*15;
+                red_circle.top  = this.circleY_3*15;
                 return red_circle;
                 
-            }
+            },
+
+            circleStyle_4(){
+                const red_circle = {};
+                red_circle.left = this.circleX_4*15;
+                red_circle.top  = this.circleY_4*15;
+                return red_circle;
+            },
+
+            circleStyle_5(){
+                const red_circle = {};
+                red_circle.left = this.circleX_5*15;
+                red_circle.top  = this.circleY_5*15;
+                return red_circle;
+            },
+
+            
         } 
-      
     }   
     
     
--- src/pages/components/myGrid.vue
+++ src/pages/components/myGrid.vue
@@ -1,57 +1,58 @@
 <template>
         <div>
             <!-- Horizontal Block 6 Square -->
-                <horizontal-grid6 class="wall-1"></horizontal-grid6>
-                <horizontal-grid6 class="wall-5"></horizontal-grid6>
-                <horizontal-grid6 class="wall-7"></horizontal-grid6>
-                <horizontal-grid6 class="wall-8"></horizontal-grid6>
-                <horizontal-grid6 class="wall-9"></horizontal-grid6>
-                <horizontal-grid6 class="wall-17"></horizontal-grid6>
-                <horizontal-grid6 class="wall-24"></horizontal-grid6>
-                <horizontal-grid6 class="wall-25"></horizontal-grid6>
-                <horizontal-grid6 class="wall-33"></horizontal-grid6>
-                <horizontal-grid6 class="wall-33"></horizontal-grid6>
-                <horizontal-grid6 class="wall-38"></horizontal-grid6>
-                <horizontal-grid6 class="wall-43"></horizontal-grid6>
+                <horizontal-grid6 class="wall_grid wall-1"></horizontal-grid6>
+                <horizontal-grid6 class="wall_grid wall-5"></horizontal-grid6>
+                <horizontal-grid6 class="wall_grid wall-7"></horizontal-grid6>
+                <horizontal-grid6 class="wall_grid wall-8"></horizontal-grid6>
+                <horizontal-grid6 class="wall_grid wall-9"></horizontal-grid6>
+                <horizontal-grid6 class="wall_grid wall-17"></horizontal-grid6>
+                <horizontal-grid6 class="wall_grid wall-24"></horizontal-grid6>
+                <horizontal-grid6 class="wall_grid wall-25"></horizontal-grid6>
+                <horizontal-grid6 class="wall_grid wall-33"></horizontal-grid6>
+                <horizontal-grid6 class="wall_grid wall-33"></horizontal-grid6>
+                <horizontal-grid6 class="wall_grid wall-38"></horizontal-grid6>
+                <horizontal-grid6 class="wall_grid wall-43"></horizontal-grid6>
 
             <!-- Horizontal Block 10 Square -->
-                <horizontal-grid10 class="wall-2"></horizontal-grid10>
-                <horizontal-grid10 class="wall-12"></horizontal-grid10>
-                <horizontal-grid10 class="wall-14"></horizontal-grid10>
-                <horizontal-grid10 class="wall-20"></horizontal-grid10>
-                <horizontal-grid10 class="wall-21"></horizontal-grid10>
-                <horizontal-grid10 class="wall-32"></horizontal-grid10>
-                <horizontal-grid10 class="wall-37"></horizontal-grid10>
-                <horizontal-grid10 class="wall-39"></horizontal-grid10>
-                <horizontal-grid10 class="wall-40"></horizontal-grid10>
-                <horizontal-grid10 class="wall-41"></horizontal-grid10>
-                <horizontal-grid10 class="wall-44"></horizontal-grid10>
-                <horizontal-grid10 class="wall-45"></horizontal-grid10>
-                <horizontal-grid10 class="wall-46"></horizontal-grid10>
+                <horizontal-grid10 class="wall_grid wall-2"></horizontal-grid10>
+                <horizontal-grid10 class="wall_grid wall-12"></horizontal-grid10>
+                <horizontal-grid10 class="wall_grid wall-14"></horizontal-grid10>
+                <horizontal-grid10 class="wall_grid wall-20"></horizontal-grid10>
+                <horizontal-grid10 class="wall_grid wall-21"></horizontal-grid10>
+                <horizontal-grid10 class="wall_grid wall-32"></horizontal-grid10>
+                <horizontal-grid10 class="wall_grid wall-37"></horizontal-grid10>
+                <horizontal-grid10 class="wall_grid wall-39"></horizontal-grid10>
+                <horizontal-grid10 class="wall_grid wall-40"></horizontal-grid10>
+                <horizontal-grid10 class="wall_grid wall-41"></horizontal-grid10>
+                <horizontal-grid10 class="wall_grid wall-44"></horizontal-grid10>
+                <horizontal-grid10 class="wall_grid wall-45"></horizontal-grid10>
+                <horizontal-grid10 class="wall_grid wall-46"></horizontal-grid10>
 
             <!-- Vertically Block -->
-                <vertically-grid class="wall-3"></vertically-grid>
-                <vertically-grid class="wall-4"></vertically-grid>
-                <vertically-grid class="wall-6"></vertically-grid>
-                <vertically-grid class="wall-10"></vertically-grid>
-                <vertically-grid class="wall-11"></vertically-grid>
-                <vertically-grid class="wall-13"></vertically-grid>
-                <vertically-grid class="wall-15"></vertically-grid>
-                <vertically-grid class="wall-16"></vertically-grid>
-                <vertically-grid class="wall-18"></vertically-grid>
-                <vertically-grid class="wall-19"></vertically-grid>
-                <vertically-grid class="wall-22"></vertically-grid>
-                <vertically-grid class="wall-23"></vertically-grid>
-                <vertically-grid class="wall-26"></vertically-grid>
-                <vertically-grid class="wall-27"></vertically-grid>
-                <vertically-grid class="wall-28"></vertically-grid>
-                <vertically-grid class="wall-29"></vertically-grid>
-                <vertically-grid class="wall-30"></vertically-grid>
-                <vertically-grid class="wall-31"></vertically-grid>
-                <vertically-grid class="wall-34"></vertically-grid>
-                <vertically-grid class="wall-35"></vertically-grid>
-                <vertically-grid class="wall-36"></vertically-grid>
-                <vertically-grid class="wall-42"></vertically-grid>
+                <vertically-grid class="wall_grid wall-3"></vertically-grid>
+                <vertically-grid class="wall_grid wall-4"></vertically-grid>
+                <vertically-grid class="wall_grid wall-6"></vertically-grid>
+                <vertically-grid class="wall_grid wall-10"></vertically-grid>
+                <vertically-grid class="wall_grid wall-11"></vertically-grid>
+                <vertically-grid class="wall_grid wall-13"></vertically-grid>
+                <vertically-grid class="wall_grid wall-15"></vertically-grid>
+                <vertically-grid class="wall_grid wall-16"></vertically-grid>
+                <vertically-grid class="wall_grid wall-18"></vertically-grid>
+                <vertically-grid class="wall_grid wall-19"></vertically-grid>
+                <vertically-grid class="wall_grid wall-22"></vertically-grid>
+                <vertically-grid class="wall_grid wall-23"></vertically-grid>
+                <vertically-grid class="wall_grid wall-26"></vertically-grid>
+                <vertically-grid class="wall_grid wall-27"></vertically-grid>
+                <vertically-grid class="wall_grid wall-28"></vertically-grid>
+                <vertically-grid class="wall_grid wall-29"></vertically-grid>
+                <vertically-grid class="wall_grid wall-30"></vertically-grid>
+                <vertically-grid class="wall_grid wall-31"></vertically-grid>
+                <vertically-grid class="wall_grid wall-34"></vertically-grid>
+                <vertically-grid class="wall_grid wall-35"></vertically-grid>
+                <vertically-grid class="wall_grid wall-36"></vertically-grid>
+                <vertically-grid class="wall_grid wall-42"></vertically-grid>
+                <vertically-grid class="wall_grid wall-47"></vertically-grid>
         </div>
 </template>
 <script>
