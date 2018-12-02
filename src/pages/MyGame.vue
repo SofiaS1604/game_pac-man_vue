e855a81ae974da94d3a65d39b86917894e8d8c5c
SofiaS1604 <SofiaS1604@yandex.ru>
2018-12-02 21:23:34 +0300

    Add files via upload

--- src/pages/MyGame.vue
+++ src/pages/MyGame.vue
@@ -224,7 +224,9 @@
                     this.x === 20 && (this.y === 9 || this.y === 8) ||
                     this.x === 30 && this.y < 8 ||
                     this.x === 35  && (this.y > 4 && this.y < 10) ||
-                    this.x === 43  && (this.y > 2 && this.y < 25)){
+                    this.x === 43  && (this.y > 2 && this.y < 25) ||
+                    this.x === 20  && (this.y > 17 && this.y < 25) ||
+                    this.x === 15   && (this.y < 18 && this.y > 9)){
 
                     }else{
                         this.x --;
@@ -279,7 +281,8 @@
                     (this.x > 32 && this.x < 43)  && this.y === 2 ||
                     this.y === 7 && (this.x > 9 && this.x < 20) ||
                     this.x > 41 && this.y === 22 ||
-                    (this.x > 7 && this.x < 13)  && this.y === 17){
+                    (this.x > 7 && this.x < 13)  && this.y === 17||
+                    (this.x > 14 && this.x < 20)  && this.y === 17 ){
 
                     }else{
                         this.y ++;
